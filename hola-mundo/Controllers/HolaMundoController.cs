a0abab670d970f9d4561849c3e6d0f6cbd5f39d9
estsban11 <estsban11@gmail.com>
2024-03-21 19:32:45 -0500

    Agregar archivos de proyecto.

---
+++ hola-mundo/Controllers/HolaMundoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hola_mundo.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class HolaMundoController : Controller
+    {
+
+        [HttpGet]
+        public  IActionResult GetHolaMundo()
+        {
+            return StatusCode(201, "Hola mundo");
+        }
+    }
+}
